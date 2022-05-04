8d74c50a9e5559ce4d1e6002f14ece00eb0bc491
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-04 12:13:41 -0700

    passed goblin and handleDeleteGoblin as props in goblin.js

--- src/Goblin.js
+++ src/Goblin.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-export default function Goblin(props) {
+export default function Goblin({ goblin, handleDeleteGoblin }) {
   return (
     // be sure you take a look at this component i'm handing you and figure out what props it will need to work correctly.
-    <div 
-      className='goblin' 
-      onClick={() => props.handleDeleteGoblin && props.handleDeleteGoblin(props.goblin.name)}>
-      <h3>{props.goblin.name}</h3>  
-      <img src="goblin.png" style={{ backgroundColor: props.goblin.color }} />
-      <p>{props.goblin.hp} HP</p>
+    <div className="goblin" onClick={() => handleDeleteGoblin && handleDeleteGoblin(goblin.name)}>
+      <h3>{goblin.name}</h3>
+      <img src="goblin.png" style={{ backgroundColor: goblin.color }} />
+      <p>{goblin.hp} HP</p>
     </div>
   );
 }
